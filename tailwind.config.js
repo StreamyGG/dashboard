1af522cf22b5a0d39191bf04030d0918117129b8
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-08-21 23:24:22 -0500

    yuhh

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],  
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          'display': 'none',
+        },
+      });
+    }
+  ],
+}
+
